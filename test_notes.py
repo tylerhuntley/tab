a7bff0b71a008fe69518625c5a0c310a9d0c1dae
tylerhuntley <thetylerhuntley@gmail.com>
2018-03-28 20:55:54 -0700

    Delete redundant top-level Note method equivalent functions

--- test_notes.py
+++ test_notes.py
@@ -96,50 +96,6 @@ class Note():
         return frets
 
 
-def name_to_num(note):
-    """ Convert standard note name into a linear numeric value """
-    value = 0
-    name = {'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11}
-    accidental = {'b': -1, '#': 1}
-    
-    try:
-        value += int(note[-1]) * 12
-        value += name[note[0]]
-        if len(note) == 3:
-            value += accidental[note[1]]
-    except KeyError:
-        print("Invalid note: ", note)
-        return None
-    except ValueError:
-        print("Invalid note: ", note)
-        return None
-    return value
-
-
-def chord_to_num(chord):
-    """ Input list of standard note names 
-    Returns list of numerical note values"""
-    nums = []
-    for note in chord:
-        num = name_to_num(note)
-        if not num is None:
-            nums.append(num)
-    return nums
-
-
-def num_to_fret(number, tuning=STD_TUNING):
-    """ Convert numerical note value to list of all fretting options
-    Returns list of tuples: (string, fret) """
-    options = []
-    if not type(number) is int:
-        return options
-    for string, value in enumerate(tuning):
-        if number >= LOW_E + value:
-            fret = number - (LOW_E + value)
-            options.append((string, fret))
-    return options
-
-
 def chord_shape(values):
     """ Input list of numerical note values
     Returns list of tuples: (string, fret) 
