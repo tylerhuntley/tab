548c0b89348270c0bfd04ddae6025b4f7c2107a8
tylerhuntley <thetylerhuntley@gmail.com>
2018-04-07 16:49:31 -0700

    Absorb exploratory image processing into new Detector() class
    Also maintains storage of image data between sessions, to avoid pricey
    reprocessing.

--- test_lines.py
+++ test_lines.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from time import time
 import itertools
+import pickle
 
 
 def detect_edges(img, sigma=0.33):
@@ -57,37 +58,70 @@ def plot(img, gray=False):
 def length(line):
     for x1, y1, x2, y2 in line:
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+    
 
+class Detector():    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)      
+        
+        # Image file info
+        self.path = 'static/'
+        self.images = ('line', 'easy', 'hard')
+        self.image_names = tuple(f'{self.path}{i}.png' for i in self.images)
+        
+        # Load image files
+        self.image_data = {}
+        for image, name in zip(self.images, self.image_names):
+            self.image_data[image] = cv2.imread(name)
+
+        # Load data, if available
+        try:
+            self.line_data = np.load('data')
+        # Generate data, if needed
+        except FileNotFoundError:
+            self.line_data = {}
+            for image, name in zip(self.images, self.image_names):
+                if not self.line_data[image]:
+                    self.line_data[image] = self.probe_image(name)
+            with open('data', 'wb') as f:
+                pickle.dump(self.line_data, f)                
+
+
+    def probe_image(self, filename):
+        data = {}
+        # Prepare color-inverted, grayscale array of image data
+        image = cv2.imread(filename)
+        if image is None:
+            print(f'{filename} not found')
+            return None
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        inverted = np.invert(gray)
+        
+        # For timing purposes
+        count = 0
+        start = time()
+        last = start
+        
+        # Parameter initialization
+        min_length = image.shape[1] * 0.5
+        params = range(0, 200, 10)
+        gaps = range(0, 100, 5)
+        cycles = len(params) * len(gaps)
+        
+        for (param, gap) in itertools.product(params, gaps):
+            lines = detect_lines(inverted, param, min_length, gap)
+            data[(param, gap)] = lines
+            
+            # Timing info
+            count += 1
+            now = time()
+            print('Cycle {} of {}: {:.2f} us, Total: {:.2f} s'.format(
+                    count, cycles, (now - last)*1000, now - start))
+            last = now
+        
+        return data
+    
 
 if __name__ == '__main__':
     
-    # Prepare color-inverted, grayscale array of image data
-    image = cv2.imread('static/line.png')
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    inverted = np.invert(gray)
-
-    # For timing purposes
-    count = 0
-    start = time()
-    last = start
-    
-    data = {}
-    staves = {}
-    min_length = image.shape[1] * 0.5  # half-width minimum
-    
-    for (param, max_gap) in itertools.product(
-            range(0, 200, 10), range(0, 100, 5)):
-
-        lines = detect_lines(inverted, param, min_length, max_gap)
-        if len(lines) == 5:
-            staves[(param, max_gap)] = lines
-            
-        data[(param, max_gap)] = lines
-        
-        # Timing info
-        count += 1
-        now = time()
-        print('Cycle {}: {:.2f} us, Total: {:.2f} s'.format(count, 
-              (now - last)*1000, now - start))
-        last = now
-                
+    d = Detector()
