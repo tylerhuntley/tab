213547ec381c56ac7f41703a01ddf998504387e0
tylerhuntley <thetylerhuntley@gmail.com>
2018-04-13 21:36:37 -0700

    Absorb global functions into Detector (sub)classes

--- test_lines.py
+++ test_lines.py
@@ -7,75 +7,79 @@ import pickle
 import unittest
 
 
-def detect_edges(img, sigma=0.33):
-    ''' '''
-    mean = np.mean(img)
-    lower = int(max(0, (1 - sigma) * mean))
-    upper = int(min(255, (1 + sigma) * mean))
-    return cv2.Canny(img, lower, upper)
-
-
-def detect_lines(img, param, min_length, max_gap):
-    ''' '''
-#    rho, theta = 1, np.pi/180  # distance and angle parameters for accumulator
-    lines = cv2.HoughLinesP(img, rho=1, theta=np.pi/180, threshold=param,
-                            minLineLength=min_length, maxLineGap=max_gap)
-    return lines
-
-
-def separate_staffs(lines):
-    ''' Return list of lists containing the lines of each staff entity
-    Currently just counts five lines at a time, should utilize numpy'''
-    staffs = []
-    for i in range(0, len(lines), 5):
-        staffs.append(lines[i:i + 5])
-    return staffs
-
-
-def slice_staff():
-    ''' Return image subarray comprising one staff line'''
-    pass
-
-
-
-def draw_lines(img, lines, color=(255, 0, 0)):
-    ''' Return img with lines drawn on it in color, default red'''
-    result = img.copy()
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(result, (x1, y1), (x2, y2), color, 1)
-    return result
-
-
-def plot(img, gray=False):
-    ''' Display img in a matplotlib plot window'''
-    plt.plot()
-    if gray:
-        plt.imshow(img, 'gray')
-    else:
-        plt.imshow(img)
-    plt.show()
-
-
-def length(line):
-    for x1, y1, x2, y2 in line:
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-
-
 class Detector():
-    key = {'line': 5, 'kumbayah': 10, 'star': 15, 'sleeves': 20}
+    key = {'line': 5, 'kumbayah': 10, 'star': 15, 'sleeves': 20, 'rosita': 40}
+    exclude = {'rosita', 'sleeves'}
     image_path = 'static/'
     data_path = 'data/'
 
-    def __init__(self, name, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
+    def __init__(self, name):
         # Image info
         self.name = name
         self.image_name = f'{self.image_path}{name}.png'
         self.image = cv2.imread(self.image_name)
         self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.inverted = np.invert(self.gray)
+
+
+    def load_data(self):
+        self.line_data = np.load(f'{self.data_path}{self.name}')
+
+    def save_data(self):
+        with open(f'{self.data_path}{self.name}', 'wb') as f:
+            pickle.dump(self.line_data, f)
+
+    @staticmethod
+    def length(line):
+        (x1, y1, x2, y2) = line
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+    @staticmethod
+    def is_horizontal(line):
+        (x1, y1, x2, y2) = line
+        return y1 == y2
+
+    @staticmethod
+    def is_vertical(line):
+        (x1, y1, x2, y2) = line
+        return x1 == x2
+
+    @staticmethod
+    def detect_edges(img, sigma=0.33):
+        mean = np.mean(img)
+        lower = int(max(0, (1 - sigma) * mean))
+        upper = int(min(255, (1 + sigma) * mean))
+        return cv2.Canny(img, lower, upper)
+
+    @staticmethod
+    def detect_lines(img, param, min_length, max_gap):
+        rho, theta = 1, np.pi/180
+        return cv2.HoughLinesP(img, rho, theta, threshold=param,
+                                minLineLength=min_length, maxLineGap=max_gap)
+
+    @staticmethod
+    def draw_lines(img, lines, color=(255, 0, 0)):
+        ''' Return image with lines drawn on it in color, default red'''
+        result = img.copy()
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(result, (x1, y1), (x2, y2), color, 1)
+        return result
+
+    @staticmethod
+    def plot(img, gray=False):
+        ''' Display image in a matplotlib plot window'''
+        plt.plot()
+        if gray:
+            plt.imshow(img, 'gray')
+        else:
+            plt.imshow(img)
+        plt.show()
+
+
+class StaffLines(Detector):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         # Load data, if available
         try:
@@ -85,15 +89,6 @@ class Detector():
             self.line_data = {}
             self.probe_image()
             self.save_data()
-
-
-    def load_data(self):
-        self.line_data = np.load(f'{self.data_path}{self.name}')
-
-
-    def save_data(self):
-        with open(f'{self.data_path}{self.name}', 'wb') as f:
-            pickle.dump(self.line_data, f)
 
 
     def probe_image(self, params=range(0, 200, 10), gaps=range(0, 100, 5)):
@@ -109,7 +104,7 @@ class Detector():
             # Don't repeat detection with duplicate parameters
             if (param, max_gap) in self.line_data:
                 continue
-            lines = detect_lines(self.inverted, param, min_length, max_gap)
+            lines = self.detect_lines(self.inverted, param, min_length, max_gap)
             # NoneType has no len(), so use empty tuples for 0 lines instead
             self.line_data[(param, max_gap)] = () if lines is None else lines
 
@@ -119,6 +114,20 @@ class Detector():
             print('{}, cycle {} of {}: {:.2f} us, Total: {:.2f} s'.format(
                     self.name, count, cycles, (now - last)*1000, now - start))
             last = now
+
+
+    def separate_staffs(lines):
+        ''' Return list of lists containing the lines of each staff entity
+        Currently just counts five lines at a time, should utilize numpy'''
+        staffs = []
+        for i in range(0, len(lines), 5):
+            staffs.append(lines[i:i + 5])
+        return staffs
+
+
+    def slice_staff():
+        ''' Return image subarray comprising one staff line'''
+        pass
 
 
     def get_params(self, n):
@@ -141,16 +150,6 @@ class Detector():
                 result.append(k)
                 print(f'{self.name}: {k} -> {len(v)}')
         return result
-
-
-    def is_horizontal(self, line):
-        (x1, y1, x2, y2) = line
-        return y1 == y2
-
-
-    def is_vertical(self, line):
-        (x1, y1, x2, y2) = line
-        return x1 == x2
 
 
     def join_h_lines(self, lines):
@@ -178,7 +177,8 @@ class TestLines(unittest.TestCase):
     def setUp(self):
         self.tests = {}
         for i in Detector.key:
-            self.tests[i] = Detector(i)
+            if i not in Detector.exclude:
+                self.tests[i] = StaffLines(i)
 
     def test_counts(self):
         for k, v in self.tests.items():
